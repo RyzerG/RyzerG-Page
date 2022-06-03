631f65103a8e549151c51f10f2c9a14076bbb968
RyzerG <75449600+RyzerG@users.noreply.github.com>
2022-06-03 11:40:05 -0400

    about changes #1
    iteration 1

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'Python', 'HTML/CSS/JS', 'MySQL', 'Node.js', 'YAML'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,21 +136,20 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hi There! My name is Rohan and I enjoy the world of technology and innovation. My
+              interest in tech started in 2015 when I decided to try make custom discord bots
+              — and after a few revisions, I was able to confidently say tech was my passion
+              and I love code &amp; CSS!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://currentmotors.ca/">an automotive startup</a>,{' '}
+              <a href="https://fiverr.com/">a freelancing company</a>,{' '}
+              <a href="https://overclockedservices.ca/">my own organization</a>,{' '}. My
+              main goals today is making open source projects, new platforms and premium digital
+              spaces at <a href="https://overclockedservices.ca/">OCS</a> for a large scale of
+              business clients.
             </p>
 
             <p>
