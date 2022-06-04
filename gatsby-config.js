004fd59f78095456ed9cb3e1af04bb2c906b202c
RyzerG <75449600+RyzerG@users.noreply.github.com>
2022-06-03 20:29:08 -0400

    name info change
    name, and other info update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Rohan Singh',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Rohan Singh is a web developer who aspires to create and build premium-level digital products.',
+    siteUrl: 'https://rohansingh.ca', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@rohan_singh24',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'RohanSingh',
+        short_name: 'RohanSingh',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
